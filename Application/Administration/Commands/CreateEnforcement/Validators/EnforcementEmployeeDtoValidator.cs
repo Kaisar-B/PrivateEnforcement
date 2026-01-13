2ac06bbba03caaba18aacb1daca39e50bf7be64b
Kaisar-B <kais_00@bk.ru>
2026-01-13 23:09:31 +0500

    partial validator for dto

--- Application/Administration/Commands/CreateEnforcement/Validators/EnforcementEmployeeDtoValidator.cs
+++ Application/Administration/Commands/CreateEnforcement/Validators/EnforcementEmployeeDtoValidator.cs
@@ -16,5 +16,35 @@ public class EnforcementEmployeeDtoValidator : AbstractValidator<EnforcementEmpl
             .WithSeverity(Severity.Error)
             .WithName("Номер паспорта работника")
             .WithMessage("Номер паспорта работника должен быть больше чем ноль");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithSeverity(Severity.Error)
+            .WithName("Имя наемного сотрудника ЧСИ")
+            .WithMessage("Имя сотрудника ЧСИ должно быть заполненно в обезательном порядке");
+
+        RuleFor(x => x.Surname)
+            .NotEmpty()
+            .WithSeverity(Severity.Error)
+            .WithName("Фамилия наемного сотрудника ЧСИ")
+            .WithMessage("Фамилия сотрудника ЧСИ должно быть заполненно в обезательном порядке");
+
+        RuleFor(x => x.Login)
+            .NotEmpty()
+            .WithSeverity(Severity.Error)
+            .WithName("Логин")
+            .WithMessage("Логин сотрудника ЧСИ должно быть заполненно в обезательном порядке");
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithSeverity(Severity.Error)
+            .WithName("Пароль")
+            .WithMessage("Пароль одноразовый должен быть присвоен");
+
+        RuleFor(x=>x.Position)
+            .NotEmpty()
+            .WithSeverity(Severity.Error)
+            .WithName("Должность")
+            .WithMessage("Должность сотрудника ЧСИ должно быть заполненно в обезательном порядке");
     }
 }
